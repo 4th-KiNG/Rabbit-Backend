33a56c8a8696fbf150684b1a0c66bb1466075b5b
Michael Sidorchenko <qternel@gmail.com>
2024-10-11 23:04:24 +0300

    temporary commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
-
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
+import { User } from "./user/user.entity";
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "postgres",
+      entities: [User],
+      synchronize: true
+    }),
+    AuthModule,
+    UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
