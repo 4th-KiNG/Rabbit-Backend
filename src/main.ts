b295551ec92ef23dd73ec2463c0523e41f09f752
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-11-30 18:53:23 +0300

    change import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import fs from "fs";
+import * as fs from "fs";
 
 async function bootstrap() {
   const httpsOptions = {
