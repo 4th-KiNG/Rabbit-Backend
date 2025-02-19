7a7bf0c296c02dbf9b7331861d77f44b8b6f9443
Michael Sidorchenko <qternel@gmail.com>
2025-02-20 00:25:41 +0300

    comment tree

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -5,12 +5,14 @@ import {
   Request,
   UseGuards,
   Get,
+  Query,
 } from "@nestjs/common";
 import { ApiTags, ApiOperation } from "@nestjs/swagger";
 import { CommentsService } from "./comments.service";
 import { CreateCommentDto } from "src/dtos/comment.dto";
 import { Request as Request_type } from "express";
 import { JwtGuard } from "src/guard/jwt.guard";
+import { ParentType } from "./comments.types";
 @ApiTags("comments")
 @Controller("comments")
 export class CommentsController {
@@ -25,11 +27,16 @@ export class CommentsController {
   }
 
   @ApiOperation({ summary: "get comment tree's level" })
-  @Get("get")
-  GetCommentTreeLevel(@Body() dto: Omit<CreateCommentDto, "text">) {
+  @Get()
+  GetCommentTreeLevel(
+    @Query("parentId") parentId: string,
+    @Query("parentType") parentType: string,
+  ) {
     return this.commentsService.getCommentTreeLevel(
-      dto.parentId,
-      dto.parentType,
+      parentId,
+      parentType.toLowerCase() == "comment"
+        ? ParentType.Comment
+        : ParentType.Post,
     );
   }
 }
